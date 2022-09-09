85e4a03d40bec7b269377a3753890d025c0abfbf
Braxton Diggs <braxtondiggs@gmail.com>
2022-09-09 18:26:49 -0400

    Add port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3333;
+
+  await app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 }
 bootstrap();
