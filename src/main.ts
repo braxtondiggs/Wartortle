9657cd8793f74dee53e5ce919dddaded7a68163e
Braxton Diggs <braxtondiggs@gmail.com>
2022-09-09 18:36:59 -0400

    Add Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const port = process.env.PORT || 3333;
 
-  await app.listen(port, () => {
+  await app.listen(port, '0.0.0.0', () => {
     console.log(`Listening on port ${port}`);
   });
 }
