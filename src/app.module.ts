691a07e5a414bfb9b3e818aebeb18583dfd9e66f
Braxton Diggs <braxtondiggs@gmail.com>
2022-09-11 19:00:36 -0400

    Add cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskService } from './task.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, TaskService]
 })
-export class AppModule { }
+export class AppModule {}
